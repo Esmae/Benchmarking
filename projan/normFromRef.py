25a0c375896ef4b46e080dd9c6610ad87c89170b
Esmae <esmae.woods@diamond.ac.uk>
2017-07-13 14:57:57 +0100

    adding scripts to use data obtained in build
    python scripts for normalisation and plotting and the bash script with
    the commands

---
+++ projan/normFromRef.py
@@ -0,0 +1,44 @@
+"""
+Normalising the benchmarking data with respect to the reference benchmark run
+"""
+
+import csv
+#set default normalisation to be 1, i.e. no normalisation
+normalise = float(1.0)
+
+#reading in the actual normalisation
+ref = open("refBench", "rb")
+reader = csv.reader(ref)
+i = 0
+for row in reader:
+    if i != 0:
+        #skipping the first row as this is the header
+        #the file should only contain 1 other row
+        normalise = row[4]
+    i = i + 1
+        
+ref.close()
+        
+fileList = ["testTen"]
+fileListNorm = ["master/projan/normData/testTen"]
+for i in range(0,len(fileList)):
+    noNorm = open(fileList[i], "rb")
+    reader = csv.reader(noNorm)
+    norm = open(fileListNorm[i], "wb")
+    writer = csv.writer(norm)
+
+
+    j = 0
+    for row in reader:
+        if j != 0:
+            #normalising the score
+            row[4] = str(float(row[4])/float(normalise))
+            #normalising the error associated with the score
+            row[5] = str(float(row[5])/float(normalise))
+        writer.writerow(row)
+        j = j + 1
+            
+    #closing the files so they are updated
+    noNorm.close()    
+    norm.close()
+
---
+++ projan/update-gh.sh
@@ -0,0 +1,49 @@
+if [ "$TRAVIS_COMMIT_MESSAGE" != "Travis build pushed to Benchmarking testing" ]; then
+ echo -e "Starting to update Benchmarking testing\n"
+#moving files created in build into home
+#moving scripts to be run into home
+  cp -R test* $HOME
+  cp -R time* $HOME
+  cp -R refBench $HOME
+  cp -R normFromRef.py $HOME
+  
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+  #cloning the relevant repo
+  git clone --quiet --branch=testing https://${TOKEN}@github.com/Esmae/Benchmarking.git  testing> /dev/null
+  
+#normalising the benchmarking data just collected
+python normFromRef.py
+
+
+#attaching the time and date to the data files
+  today=$(date +%Y-%m-%d_%H-%M)
+  mv testTen testTen."$today"
+  mv testing/projan/normData/testTenNorm master/projan/normData/testTenNorm."$today"  
+  
+#putting the files in local repo
+  cp -Rf $HOME/test* master/projan/data
+  cp -Rf $HOME/refBench master/projan
+  
+
+  
+  #need to change the python script if change the name of the folder the clone is going into (currently it's called master)
+  #calls the python script that creates the 'with time' plot
+  python timePlot.py
+
+  #putting the new 'with time' plot in the repo (possibly overwritting the latest one)
+  cp -Rf TimePlot.png master/projan/figures
+  cp -Rf TimePlotTen.png master/projan/figures
+ 
+  
+  cd master
+ #adding the new files and changing files so they are ready to commit 
+  git add -f --ignore-removal .
+#commiting the added changes
+  git commit -m "Travis build pushed to Matrix3 master"
+#pushes the changes the github on the master branch of Matrices3
+  git push -fq https://${TOKEN}@github.com/Esmae/Benchmarking.git testing > /dev/null
+
+  echo -e "Success? \n"
+fi
