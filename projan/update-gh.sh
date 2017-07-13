75a3f7bbe7fdd0371d54c1372bed85760b98bd60
Esmae <esmae.woods@diamond.ac.uk>
2017-07-13 15:18:19 +0100

    Update update-gh.sh
    debugging: correcting path to files, branch is no longer master but testing

--- projan/update-gh.sh
+++ projan/update-gh.sh
@@ -19,29 +19,29 @@ python normFromRef.py
 #attaching the time and date to the data files
   today=$(date +%Y-%m-%d_%H-%M)
   mv testTen testTen."$today"
-  mv testing/projan/normData/testTenNorm master/projan/normData/testTenNorm."$today"  
+  mv testing/projan/normData/testTenNorm testing/projan/normData/testTenNorm."$today"  
   
 #putting the files in local repo
-  cp -Rf $HOME/test* master/projan/data
-  cp -Rf $HOME/refBench master/projan
+  cp -Rf $HOME/test* testing/projan/data
+  cp -Rf $HOME/refBench testing/projan
   
 
   
-  #need to change the python script if change the name of the folder the clone is going into (currently it's called master)
+  #need to change the python script if change the name of the folder the clone is going into (currently it's called testing)
   #calls the python script that creates the 'with time' plot
   python timePlot.py
 
   #putting the new 'with time' plot in the repo (possibly overwritting the latest one)
-  cp -Rf TimePlot.png master/projan/figures
-  cp -Rf TimePlotTen.png master/projan/figures
+  cp -Rf TimePlot.png testing/projan/figures
+  cp -Rf TimePlotTen.png testing/projan/figures
  
   
-  cd master
+  cd testing
  #adding the new files and changing files so they are ready to commit 
   git add -f --ignore-removal .
 #commiting the added changes
-  git commit -m "Travis build pushed to Matrix3 master"
-#pushes the changes the github on the master branch of Matrices3
+  git commit -m "Travis build pushed to Benchmarking testing"
+#pushes the changes the github on the testing branch of Benchmarking
   git push -fq https://${TOKEN}@github.com/Esmae/Benchmarking.git testing > /dev/null
 
   echo -e "Success? \n"
