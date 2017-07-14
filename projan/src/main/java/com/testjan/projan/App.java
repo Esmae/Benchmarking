c4d80ab97bc9ca79d90c2278b2bb37b904f30ebe
Esmae <esmae.woods@diamond.ac.uk>
2017-07-14 10:17:15 +0100

    Implementing a JUnit test for tensorDotProduct

--- projan/src/main/java/com/testjan/projan/App.java
+++ projan/src/main/java/com/testjan/projan/App.java
@@ -17,7 +17,6 @@ public class App
         
         
         Dataset dataset1 = DatasetFactory.createFromObject(new double[] { 0,1,2, 3, 4, 5});
-       dataset1.set(2, 0);
         
         dataset1 = dataset1.reshape(1,3,2);
         Dataset dataset2 = DatasetFactory.createFromObject(new double[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23});
@@ -25,8 +24,15 @@ public class App
         
         Dataset result = LinearAlgebra.tensorDotProduct(dataset1, dataset2,new int[]{1,2},new int[]{2,1});
         System.out.println("result " + result.toString(true));
-       
+        System.out.println("shape of result: " + Arrays.toString(result.getShape()));
+        result = result.reshape(4);
+        System.out.println("shape of result: " + Arrays.toString(result.getShape()));
+        System.out.println("result " + result.toString(true));
         
+        for(int i=0;i<4;i++){
+        	System.out.println(result.getDouble(i));
+        }
+
         
      // Print the output:
      System.out.println("shape of dataset: " + Arrays.toString(dataset2.getShape()));
--- projan/src/test/java/com/testjan/projan/TestTensorDot.java
+++ projan/src/test/java/com/testjan/projan/TestTensorDot.java
@@ -1,5 +1,40 @@
 package com.testjan.projan;
 
+
+
+import org.eclipse.january.dataset.Dataset;
+import org.eclipse.january.dataset.DatasetFactory;
+import org.eclipse.january.dataset.LinearAlgebra;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class TestTensorDot {
 
+	private static Dataset dataset1;
+	private static Dataset dataset2;
+	@BeforeClass
+	public static void setUpClass(){
+		//Code executed before the first test method
+		dataset1 = DatasetFactory.createRange(6);
+		dataset2 = DatasetFactory.createRange(24);
+				
+		dataset1 = dataset1.reshape(1,3,2);
+		dataset2 = dataset2.reshape(4,2,3);
+	}
+	
+	private double[] expect1DTensor = {50,140,230,320};
+	
+	@Test
+	public void testTensorDotProduct(){
+		Dataset result = LinearAlgebra.tensorDotProduct(dataset1, dataset2, new int[] {1, 2},new int[] {2,1});
+		result = result.reshape(4);
+		for(int i=0;i<4;i++){
+			Assert.assertEquals(expect1DTensor[i], result.getDouble(i),0.0001);
+		}
+	
+		
+	}
+	
+	
 }
