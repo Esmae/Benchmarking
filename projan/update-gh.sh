a214fb48b1d74be21caa2328c803aa3cea495915
Esmae <esmae.woods@diamond.ac.uk>
2017-07-14 11:52:18 +0100

    Update update-gh.sh
    trying to run with python2.7

--- projan/update-gh.sh
+++ projan/update-gh.sh
@@ -15,7 +15,7 @@ if [ "$TRAVIS_COMMIT_MESSAGE" != "Travis build pushed to Benchmarking gh-pages" 
   
 #normalising the benchmarking data just collected
 echo -e "calling normFromRef"
-python normFromRef.py
+python2.7 normFromRef.py
 
 
 #attaching the time and date to the data files
@@ -33,7 +33,7 @@ python normFromRef.py
   #need to change the python script if change the name of the folder the clone is going into (currently it's called gh-pages)
   #calls the python script that creates the 'with time' plot
   echo -e "calling timePlot"
-  python timePlot.py
+  python2.7 timePlot.py
 
   #putting the new 'with time' plot in the repo (possibly overwritting the latest one)
   cp -Rf TimePlot.png gh-pages/projan/figures
