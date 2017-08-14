9affd0ab6b76fe72a348ef25627dc0f4d6aedbef
Esmae <esmae.woods@diamond.ac.uk>
2017-08-14 10:58:32 +0100

    Changing to a log plot

--- projan/plotWithSize.py
+++ projan/plotWithSize.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib as mpl
 mpl.use("Agg")
 from matplotlib import pyplot as mplot
@@ -5,13 +6,13 @@ import csv
 
 def plot_size(sizeData, savefigure):
     #declaring the lists
-    listScore = []
+    listScoreLog = []
     listPar = []
     with open(sizeData) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             #storing the values in the Score and Parameters columns in lists
-            listScore.append(row['Score'])
+            listScore.append(np.log(row['Score']))
             listPar.append(row['Param: S'])
     #plotting the graph
     mplot.plot(listPar,listScore,'k')
